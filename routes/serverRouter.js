5d31e3f07d445579ead496a827248e57dcefc711
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2023-04-08 23:37:33 +0530

    Delete routes directory

--- routes/serverRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const UserController = require("../models/serverModel");
-
-app.post("/admin/reset", UserController.reset);
-
-module.exports = app;
